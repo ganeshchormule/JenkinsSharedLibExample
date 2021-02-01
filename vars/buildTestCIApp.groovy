c916b9ea246d4c8236985c9f695aec12ea3688a7
ganeshchormule <ganeshchormule@gmail.com>
2021-02-01 18:29:46 +0530

    Update buildTestCIApp.groovy

--- vars/buildTestCIApp.groovy
+++ vars/buildTestCIApp.groovy
@@ -1,20 +1,4 @@
 def call() {
-    node {
-        git url: "https://github.com/ganeshchormule/JenkinsSharedLibExample"
-
-        stage("Install") {
-            bat "echo install"
-        }
-
-        stage("Test") {
-            bat "echo test"
-        }
-
-        stage("Deploy") {
-            
-          bat "echo deploy"
-            
-        }
-        
-    }
+   
+   bat "echo Testing shared lib example"
 }
